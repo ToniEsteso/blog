c65b131c528b4564f1fb12732c14d69b44f2ef98
Toni Esteso <toniesteso97@gmail.com>
2026-04-26 03:27:19 +0200

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://yourusername.github.io',
-
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+    },
   },
 });
