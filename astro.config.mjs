85f306f1eca3fe7af1fd346fbbe6f38fe3ab2051
Toni Esteso <toniesteso97@gmail.com>
2026-04-26 02:59:55 +0200

    Moved to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://yourusername.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
