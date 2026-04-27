f7373f6ae756b57edb3777114ac61545354542c1
Toni Esteso <toniesteso97@gmail.com>
2026-04-27 23:40:44 +0200

    Added some motion to the content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://yourusername.github.io',
   integrations: [sitemap()],
+  prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
